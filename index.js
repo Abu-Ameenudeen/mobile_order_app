d8f28df7ea6f58aa61dca6e8d27b4e87eb31f027
Abu Ameen <113390687+Abu-Ameenudeen@users.noreply.github.com>
2026-02-26 22:22:44 +0530

    Add menu item rendering
    written by me without use of chatGPT

---
+++ index.js
@@ -0,0 +1,26 @@
+import { menuArray } from './data.js'
+
+const menuItemsArea = document.getElementById('menu-items-area') 
+
+const getItemsFromArr = menuArray.map( (menu) => {
+     return `
+     <div class="img-text-btn"> 
+        <div class="img-n-text"> 
+            <img src="./images/${menu.img}" class="item-img" alt="${menu.name} image"> 
+            <div class="menu-discrptn"> 
+                <h2>${menu.name}</h2> 
+                <p class="ingredients">${menu.ingredients}</p> 
+                <h3>$${menu.price}</h3> 
+            </div> 
+        </div> 
+        <button class="add-btn" data-id="${menu.id}">+</button> 
+    </div> 
+    ` 
+}
+).join('') 
+
+function render() { 
+    menuItemsArea.innerHTML = getItemsFromArr 
+} 
+
+render()
